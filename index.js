93063378f62523a41fe491766e074fe7b31adaee
Cierra-McDonald <cierralauren15@gmail.com>
2021-03-20 21:29:30 -0700

    updating database

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const knex = require('knex')
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-closed-49502',
       user : 'cierramcdonald',
       password : '',
       database : 'smart-brain'
