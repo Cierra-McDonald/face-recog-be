89ff967fd291c96bc98a7a7b6a369c529074bfea
Cierra-McDonald <cierralauren15@gmail.com>
2021-03-20 20:22:22 -0700

    port added

--- index.js
+++ index.js
@@ -39,9 +39,9 @@ app.put('/image', (req, res) => { updateImage.handleImageUpdate(req, res, db)})
 
 app.post('/imageurl', (req, res) => { imageUrl.handleAPICall(req, res)})
 
-const PORT = process.env.PORT
-app.listen(PORT, ()=> { 
-    console.log(`app is running on port ${PORT}`)
+
+app.listen(process.env.PORT || 3000, ()=> { 
+    console.log(`app is running on port ${process.env.PORT}`);
 })
 
 
